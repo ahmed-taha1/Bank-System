260748f1dc5fa080d5e5f2be4506a1bd4172a635
Ahmed Taha <101575320+ahmed-taha1@users.noreply.github.com>
2022-11-15 01:18:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
-#include <iostream>
+//#include <iostream>
 #include "Client.h"
 #include "BankAccount.h"
 #include "SavingAccount.h"
 #include "BankApplication.h"
 using namespace std;
+
 int main() {
     BankApplication bankSystem;
     bankSystem.run();
 
-   // SavingAccount account("100",100);
-
-    //Client c("ISMAIL","CAIRO","01157077022",&account);
-
-    //    c.displayInfo();
     return 0;
 }
